a81b2a55eb42d14f733ab357657bfc17a7a3256e
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-20 10:50:39 -0500

    progress

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-var links = document.getElementsByTagName('a')
+var array = [];
+var links = document.getElementsByTagName("a");
+for(var i=0; i<links.length; i++) {
+    array.push(links[i].href);
+}
+
+console.log(array)
